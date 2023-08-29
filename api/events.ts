4aa76e1c31fbf745b9797a5872ac403d57560779
Kyle Schwartz <kyleschwartz2000@gmail.com>
2023-08-29 10:51:04 -0400

    feat: allow passing query params Also removed upstash

--- api/events.ts
+++ api/events.ts
@@ -1,79 +1,74 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 import fetch from "cross-fetch";
-import { parse } from "ical.js";
+import ical from "ical.js";
 import { DateTime } from "luxon";
-import { Redis } from "@upstash/redis";
 
 export default async (request: NextApiRequest, response: NextApiResponse) => {
-	const { id, voltage, upstash_url, upstash_token } = request.body;
+    const { id } = request.body || request.query;
 
-	if (voltage && upstash_url && upstash_token)
-		await new Redis({ url: upstash_url, token: upstash_token }).set(
-			DateTime.now().toISO(),
-			voltage
-		);
+    if (!id) throw new Error("Invalid ID!");
 
-	const res = await fetch(
-		`https://api.ticktick.com/pub/calendar/feeds/${id}`
-	);
+    const res = await fetch(
+        `https://api.ticktick.com/pub/calendar/feeds/${id}`
+    );
 
-	if (res.status >= 400) throw new Error("Bad response from server");
+    if (res.status >= 400) throw new Error("Bad response from server!");
 
-	const raw_ical = await res.text();
+    const raw_ical = await res.text();
 
-	const events: Event[] = parse(raw_ical)[2]
-		.map(([, event]: [string, any[], any[]]) =>
-			event.reduce(
-				(props, prop) => ({ ...props, [prop[0]]: prop.slice(1) }),
-				{}
-			)
-		)
-		.map(({ dtstart, dtend, summary, description }: RawEvent) => ({
-			start: date(dtstart[2]),
-			end: dtend && date(dtend?.[2]),
-			title: summary[2],
-			description: description[2],
-			TZ: dtstart[0]?.tzid,
-		}));
+    const events: Event[] = ical
+        .parse(raw_ical)[2]
+        .map(([, event]: [string, any[], any[]]) =>
+            event.reduce(
+                (props, prop) => ({ ...props, [prop[0]]: prop.slice(1) }),
+                {}
+            )
+        )
+        .map(({ dtstart, dtend, summary }: RawEvent) => ({
+            start: date(dtstart[2]),
+            end: dtend && date(dtend?.[2]),
+            title: summary[2],
+            TZ: dtstart[0]?.tzid,
+        }));
 
-	events.sort(({ start: a }, { start: b }) => (a < b ? -1 : 1));
+    events.sort(({ start: a }, { start: b }) => (a < b ? -1 : 1));
 
-	const TZ = events?.[0]?.TZ ?? "America/New_York";
-	const timestamp = DateTime.now().setZone(TZ);
+    const TZ = events?.[0]?.TZ ?? "America/New_York";
+    const timestamp = DateTime.now().setZone(TZ);
 
-	const data = {
-		timestamp,
-		events,
-	};
+    const data = {
+        timestamp,
+        events,
+    };
 
-	response.end(JSON.stringify(data, undefined, 2));
+    response.end(JSON.stringify(data, undefined, 2));
 };
 
 const date = (d: string) =>
-	DateTime.fromISO(d, { zone: "utc" }).toISO().slice(0, -1);
+    DateTime.fromISO(d, { zone: "utc" }).toISO().slice(0, -1);
 
 type EventProps = [Props, string, string];
 
 interface Props {
-	tzid?: string;
+    tzid?: string;
 }
 
 interface RawEvent {
-	dtstamp: EventProps;
-	dtstart: EventProps;
-	dtend?: EventProps;
-	summary: EventProps;
-	uid: EventProps;
-	description: EventProps;
-	sequence: [Props, string, number];
-	tzid: EventProps;
-	rrule?: [Props, string, object];
+    dtstamp: EventProps;
+    dtstart: EventProps;
+    dtend?: EventProps;
+    summary: EventProps;
+    uid: EventProps;
+    description: EventProps;
+    sequence: [Props, string, number];
+    tzid: EventProps;
+    rrule?: [Props, string, object];
 }
 
 interface Event {
-	start: string;
-	end?: string;
-	title: string;
-	description: string;
-	TZ?: string;
+    start: string;
+    end?: string;
+    title: string;
+    description: string;
+    TZ?: string;
 }
